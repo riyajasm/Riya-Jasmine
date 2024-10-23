3de5cff0cacbb69bbac11e894c099981ab74035d
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-10-23 21:21:05 +0530

    introduction 1js

---
+++ into.js
@@ -0,0 +1,11 @@
+alert("helo rija jasmine");
+console.log("code..loking lie a wow...")
+var a =prompt("enter u r number")
+var isTrue=confirm("i want to blast computer")
+if(isTrue){
+    console.log("blasted")
+}
+else{
+    console.log("not blasted")
+}
+console.log("the number is "+a)
