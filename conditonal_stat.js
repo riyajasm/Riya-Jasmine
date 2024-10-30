01f488600c2f15f029919a25480ded883119da23
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-10-30 21:24:56 +0530

    conditional statements in js

---
+++ conditonal_stat.js
@@ -0,0 +1,20 @@
+let age=0;
+// if-else
+// if(age>18)
+//     {
+//         console.log("u have a right to vote");
+//     }
+//     else{
+//         console.log("u not have a right to vote");
+//     }
+   if(age>18)
+        {
+            console.log("u have a right to vote");
+        }
+        else if(age==10){
+            console.log("u not have a right to vote");
+        } 
+        else{
+            console.log("u r dead");
+        }
+       
