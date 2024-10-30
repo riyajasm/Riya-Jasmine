45cb487e53b6498ded6d93a2b2ffe5583a92a34d
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-10-30 21:23:56 +0530

    operators concept

---
+++ operators.js
@@ -0,0 +1,11 @@
+let a=10;
+let b=20;
+//let c=a+b;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log((a-8)**(b-19));
+let age=9;
+(age<10)?console.log("u r a kid"):console.log('u r adult');
