15ef4d07d0ec211446eb43cbf4370d1467979cd5
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-10-30 21:24:19 +0530

    loops in js

---
+++ loops.js
@@ -0,0 +1,27 @@
+//to print 1 to 10 numbers
+for(i=0;i<10;i++)
+{
+    console.log(i);
+}
+let obj={
+    namme:"riya",
+    role:"web developer"
+}
+for (const key in obj) {
+   console.log(key);
+   console.log(obj[key]);
+}
+for (const element of "riya") {
+    console.log(element);
+}
+let l=20;
+while(l<30){
+    console.log(l);
+    l++;
+}
+//console.log(do while);
+let r=50;
+do{
+    console.log(r);
+    r++;
+}while(r<50)
