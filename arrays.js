1c92f0414d3e60ce59b0979f74085ffccdfd850f
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-11-02 22:06:59 +0530

    arrays concept

---
+++ arrays.js
@@ -0,0 +1,37 @@
+let a=[1,2,4,7,10];
+//console.log(a);
+a[0]=25;
+console.log(a);
+//console.log(a[10]);
+console.log(a[3]);
+console.log(typeof a);
+console.log(a.length);
+console.log(a.toString());
+console.log(a.join(" # "));
+console.log(a.pop());
+console.log(a);
+console.log(a.push(22));
+console.log(a);
+console.log(a.shift());
+console.log(a);
+console.log(a.unshift(28));
+console.log(a);
+//console.log(delete a[2]);
+console.log(a);
+let b=[99,66,88];
+let c=["riya","jaya","suneetha"];
+console.log(a.concat(b,c));
+console.log(c.sort());
+console.log(a.splice(1,2,22,33,44,55));
+console.log(a);
+console.log(a.splice(1,5));
+console.log(a);
+console.log(a.slice(1,5));
+console.log(a);
+console.log(a.reverse());
+console.log(Array.from("riya"));
+
+//console.log(a);
+
+
+
