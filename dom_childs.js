6a3793f55ec8d9f3c9a69804e0ea8ebdda87e969
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-11-14 22:14:39 +0530

    parent,children&siblings

---
+++ dom_childs.js
@@ -0,0 +1,26 @@
+console.log("helo...");
+alert("welcome riya");
+document.body.firstChild[1]
+document.body.childnode
+document.body.childNodes
+
+document.body.childNodes[1].children
+
+document.body.firstElementChild
+
+document.body.firstElementChild.childNodes[2]
+
+document.body.firstElementChild.childNodes
+
+document.body.firstElementChild.children
+ocument.body.firstChild.parent
+
+document.body.firstChild.parentNode
+
+document.body.firstChild.parentElement
+
+document.body.firstElementChild.children
+
+document.body.firstElementChild.children[2].nextElementSibling
+
+document.body.childNodes[3].rows//tale rows
