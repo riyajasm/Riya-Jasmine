3639692058bb26c8061f1d634796c38d6a32f834
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-12-25 20:23:52 +0530

    nodejs concept

---
+++ js_vs_ecmaScript2.js
@@ -0,0 +1,15 @@
+export const a=1
+export const b=2
+export const c=3
+export const d=4
+
+let o={
+    r:25,j:10
+}
+export default o;
+module.exports={
+    q:1,
+    w:10
+}
+// let z=25
+// Module.exports=z
