f38c197afa5c9f9cfdf83a1c136d61e66b8d93ad
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2024-12-25 20:23:34 +0530

    nodejs concept

---
+++ js_vs_ecmaScript.js
@@ -0,0 +1,10 @@
+console.log("hello")
+import {a,d} from "./js_vs_ecmaScript2.js"
+console.log(a,d)
+import riya from "./js_vs_ecmaScript2.js"
+console.log(riya)
+
+    
+// const r=require("./js_vs_ecmaScript2.js")
+// // console.log(r, __dirname, __filename)
+// console.log(r)
