90721e0e2c538bbb3f8e54dae73f474c3bb84de0
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2025-12-24 16:43:44 +0530

    finding path by importing use some functions

---
+++ finding_path.js
@@ -0,0 +1,5 @@
+import path from "path"
+let mypath=" C:\\Users\\DEL\\OneDrive\\Documents\\Riya web development\\node js concepts\\file path  nodejs fs practise"
+console.log(path.extname(mypath))
+console.log(path.dirname(mypath))
+console.log(path.basename(mypath))
