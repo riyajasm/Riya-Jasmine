8c2c9100789a3c5306dbab6d5d8f493b5702bdb3
riyajasm <228a1a05e2@risekrishnasaiprakasam.edu.in>
2025-12-25 21:24:55 +0530

    middleware concept in express js

--- blog.js
+++ blog.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+// Middleware that is specific to this router
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+
 // define the home page route
 router.get('/', (req, res) => {
-  res.send('Blog home page')
+  res.send('Blogs home page')
 })
 
 // define the about route
 router.get('/about', (req, res) => {
-  res.send('About blog')
-})
-
-// define the about route
-router.get('/blogpost/:slug', (req, res) => {
-  res.send(`fetch the blogpost for ${req.params.slug}`)
+  res.send('About blogs')
 })
 
 module.exports = router
